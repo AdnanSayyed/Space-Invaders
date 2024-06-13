f39a8311cc74a6985f475a63e24554a9384d6795
Adnan Sayyed <adnan.sayyed.1947@gmail.com>
2024-06-13 12:50:23 +0530

    created game service header and cpp file

---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,35 @@
+#include "/Adnan/SFML PROJECTS/Space-Invaders/Space-Invaders/Header/GameService.h";
+
+void GameService::initialize() {
+	//initialize things
+}
+
+void GameService::destroy() {
+	//clear resources
+}
+
+GameService::GameService() {
+	//constructor
+}
+
+GameService::~GameService() {
+	//destructor
+}
+
+void GameService::ignite() {
+	//start the game
+}
+
+void GameService::update() {
+	//update game logic and state
+}
+
+
+void GameService::render() {
+	//render each frame
+}
+
+bool GameService::isRunning() {
+	//game is running or not
+	return false;
+}
