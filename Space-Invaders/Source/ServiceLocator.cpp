1903147efbf54b5a3c771cb9272e613a54497625
Adnan Sayyed <adnan.sayyed.1947@gmail.com>
2024-06-14 13:56:22 +0530

    created service locator

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,21 @@
+#include "/Adnan/SFML PROJECTS/Space-Invaders/Space-Invaders/Header/ServiceLocator.h"
+
+void ServiceLocator::createServices()
+{
+}
+
+void ServiceLocator::clearAllServices()
+{
+}
+
+void ServiceLocator::initialize()
+{
+}
+
+void ServiceLocator::update()
+{
+}
+
+void ServiceLocator::render()
+{
+}
