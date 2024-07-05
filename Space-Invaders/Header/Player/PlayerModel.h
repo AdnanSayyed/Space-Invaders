15dd4ff7bc5c452f55f9d60c92790e47d6a6473a
Adnan Sayyed <adnan.sayyed.1947@gmail.com>
2024-07-05 20:17:38 +0530

    Implement main menu sprites

--- Space-Invaders/Header/Player/PlayerModel.h
+++ Space-Invaders/Header/Player/PlayerModel.h
@@ -14,7 +14,7 @@ namespace Player
     {
     private:
 
-        const sf::Vector2f initial_player_position = sf::Vector2f(500.f, 500.f);
+        const sf::Vector2f initial_player_position = sf::Vector2f(750.f, 650.f);
         sf::Vector2f player_position;
         PlayerState player_state;
         int player_score;
@@ -22,10 +22,10 @@ namespace Player
 
     public:
 
-        const sf::Vector2f left_most_position = sf::Vector2f(50.f, 0.f);
-        const sf::Vector2f right_most_position = sf::Vector2f(700.f, 0.f);
+        const sf::Vector2f left_most_position = sf::Vector2f(50.f, 950.f);
+        const sf::Vector2f right_most_position = sf::Vector2f(1800.f, 950.f);
 
-        const float player_movement_speed = 200.0f;
+        const float player_movement_speed = 350.0f;
 
         PlayerModel();
         ~PlayerModel();
